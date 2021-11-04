ce9f739b01ed4667bc03aadb520f4635bfc9c7c5
Manoela Cunha <manoela_fg@hotmail.com>
2021-11-04 09:24:10 -0300

    style(pages): add do logotipo

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,9 +1,14 @@
 import SignIn from "../../components/SignIn";
 import { ContainerLogin } from "./styles";
+import { Box } from "@material-ui/core";
+import img from "../../assets/Mask Group.png";
 
 const Login = () => {
   return (
     <ContainerLogin>
+      <Box style={{ margin: "30px" }}>
+        <img src={img} alt="logo" width="200" />
+      </Box>
       <SignIn />
     </ContainerLogin>
   );
--- src/pages/Register/index.tsx
+++ src/pages/Register/index.tsx
@@ -1,9 +1,14 @@
 import SignUp from "../../components/SignUp";
 import { ContainerRegister } from "./styles";
+import { Box } from "@material-ui/core";
+import img from "../../assets/Mask Group.png";
 
 const Register = () => {
   return (
     <ContainerRegister>
+      <Box style={{ margin: "30px" }}>
+        <img src={img} alt="logo" width="200" />
+      </Box>
       <SignUp />
     </ContainerRegister>
   );
